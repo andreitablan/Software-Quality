ecda7df3cf1c02116c2ebe8d18274d44a4695d35
Adrian-Mihai Onofrei <59933980+WoozyDeveloper@users.noreply.github.com>
2024-04-29 16:44:22 +0300

    jmp

--- ProjectSQ/Services/ProcessorService.cs
+++ ProjectSQ/Services/ProcessorService.cs
@@ -5,6 +5,9 @@ namespace ProjectSQ.Services
 {
     public class ProcessorService : IProcessorService
     {
+        private string currentInstruction; // Represents the current instruction being executed
+
+
         public void Addition(string reg1, string reg2)
         {
             Processor.registerDictionary[reg1] += Processor.registerDictionary[reg2];
@@ -76,12 +79,44 @@ namespace ProjectSQ.Services
             {
                 Console.Write("Error: Other case in Compare function");
             }
-            throw new NotImplementedException();
         }
 
         public void Compare(string reg1, int val)
         {
-            throw new NotImplementedException();
+            if (Processor.registerDictionary[reg1] == val)
+            {
+                Processor.Equal = true;
+                Processor.LessEqual = true;
+                Processor.GreaterEqual = true;
+
+                Processor.NotEqual = false;
+                Processor.Less = false;
+                Processor.Greater = false;
+            }
+            else if (Processor.registerDictionary[reg1] < val)
+            {
+                Processor.Equal = false;
+                Processor.LessEqual = false;
+                Processor.GreaterEqual = false;
+
+                Processor.NotEqual = true;
+                Processor.Less = true;
+                Processor.Greater = false;
+            }
+            else if (Processor.registerDictionary[reg1] > val)
+            {
+                Processor.Equal = false;
+                Processor.LessEqual = false;
+                Processor.GreaterEqual = false;
+
+                Processor.NotEqual = true;
+                Processor.Less = false;
+                Processor.Greater = true;
+            }
+            else
+            {
+                Console.Write("Error: Other case in Compare function");
+            }
         }
 
         public void Division(string reg1, string reg2)
@@ -96,42 +131,49 @@ namespace ProjectSQ.Services
             throw new NotImplementedException();
         }
 
+
         public void Jump(string label)
         {
-            throw new NotImplementedException();
+            for(int index = 0; index < Processor.internalMemory.Length; index++)
+                if (Processor.internalMemory[index].Equals(label))
+                    Processor.currentInstruction = index;
         }
 
-        #region poate nu ne trebuie toate astea
         public void JumpIfEqual(string label)
         {
-            throw new NotImplementedException();
-        }
-
-        public void JumpIfGreaterThan(string label)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void JumpIfGreaterThanOrEqual(string label)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void JumpIfLessThan(string label)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void JumpIfLessThanOrEqual(string label)
-        {
-            throw new NotImplementedException();
+            if (Processor.Equal)
+                Jump(label);
         }
 
         public void JumpIfNotEqual(string label)
         {
-            throw new NotImplementedException();
+            if (Processor.NotEqual)
+                Jump(label);
         }
-        #endregion 
+
+        public void JumpIfLessThan(string label)
+        {
+            if (Processor.Less)
+                Jump(label);
+        }
+
+        public void JumpIfGreaterThan(string label)
+        {
+            if (Processor.Greater)
+                Jump(label);
+        }
+
+        public void JumpIfLessThanOrEqual(string label)
+        {
+            if (Processor.LessEqual)
+                Jump(label);
+        }
+
+        public void JumpIfGreaterThanOrEqual(string label)
+        {
+            if (Processor.GreaterEqual)
+                Jump(label);
+        }
 
 
         public void Multiplication(string reg1, string reg2)
@@ -165,6 +207,11 @@ namespace ProjectSQ.Services
 
         public void Push(string reg)
         {
+            if (Processor.currentStackPointer < Processor.maximumSize)
+            {
+                //Processor.programData[Processor.currentStackPointer] = reg;
+                //Processor.currentStackPointer++;
+            }
             throw new NotImplementedException();
         }
 
