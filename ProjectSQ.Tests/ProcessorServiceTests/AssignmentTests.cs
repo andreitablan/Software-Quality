272af4058dc9df01c9c59d1be5c56ac364188b18
Fanaru Victor <fanaruvictor01@gmail.com>
2024-05-14 22:55:37 +0300

    exemple of tests

---
+++ ProjectSQ.Tests/ProcessorServiceTests/AssignmentTests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using ProjectSQ.Interfaces.Memory;
+using ProjectSQ.Interfaces.Processor;
+using ProjectSQ.Models;
+using ProjectSQ.Services;
+
+namespace ProjectSQ.Tests.ProcessorServiceTests
+{
+    public class AssignmentTests
+    {
+        private readonly IProcessorService sut;
+
+        public AssignmentTests()
+        {
+            var mockMemoryService = new Mock<IMemoryService>();
+
+            var mockHubContext = new Mock<IHubContext<RealTimeHub>>();
+
+            sut = new ProcessorService(mockHubContext.Object, mockMemoryService.Object);
+        }
+
+        [Fact]
+        public void Assignment_Should_Update_Register_Value()
+        {
+            // Arrange
+            var operandOne = "reg1";
+            var operandTwo = "123";
+            Processor.InitProcessor();
+
+            // Act
+            var result = sut.Assignment(operandOne, operandTwo);
+
+            // Assert
+            result.Should().Be(true);
+            Processor.registerDictionary[operandOne].Should().Be(123);
+        }
+
+        [Fact]
+        public void Assignment_Should_Return_False_With_Constant_Operand()
+        {
+            // Arrange
+            var operandOne = "123";
+            var operandTwo = "456";
+            Processor.InitProcessor();
+
+            // Act
+            var result = sut.Assignment(operandOne, operandTwo);
+
+            // Assert
+            result.Should().Be(false);
+        }
+
+        [Fact]
+        public void Assignment_Should_Write_Value_To_Memory_Location()
+        {
+            // Arrange
+            var operandOne = "mem[100]";
+            var operandTwo = "789";
+            Processor.InitProcessor();
+            Memory.InitMemory();
+
+            // Act
+            var result = sut.Assignment(operandOne, operandTwo);
+
+            // Assert
+            Assert.True(result);
+            var indexOperandOne = 100;
+
+            (Memory.programData[indexOperandOne] + (Memory.programData[indexOperandOne + 1] << 8)).Should().Be(789);
+        }
+
+        [Fact]
+        public void Assignment_Should_Return_False_For_Location_Beyond_Keyboard_Buffer()
+        {
+            // Arrange
+            var operandOne = "mem[60001]";
+            var operandTwo = "999";
+            Processor.InitProcessor();
+            Memory.InitMemory();
+
+            // Act
+            var result = sut.Assignment(operandOne, operandTwo);
+
+            result.Should().Be(false);
+        }
+    }
+}
---
+++ ProjectSQ.Tests/ProcessorServiceTests/ExecuteFileTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Moq;
+using ProjectSQ.Interfaces.Memory;
+using ProjectSQ.Interfaces.Processor;
+using ProjectSQ.Models;
+using ProjectSQ.Services;
+
+namespace ProjectSQ.Tests.ProcessorServiceTests
+{
+    public class ExecuteTests
+    {
+
+        private readonly IProcessorService sut;
+        private readonly Mock<IMemoryService> mockMemoryService;
+
+
+        public ExecuteTests()
+        {
+
+            //folosit pentru chestii general valabile la toate testele
+            mockMemoryService = new Mock<IMemoryService>();
+            mockMemoryService.Setup(x => x.LoadMemoryData()).Returns(new ResultMemory(Memory.programData));
+
+            var mockClients = new Mock<IHubClients>();
+            var mockAllClients = new Mock<IClientProxy>();
+            mockClients.SetupGet(clients => clients.All).Returns(mockAllClients.Object);
+
+            var mockHubContext = new Mock<IHubContext<RealTimeHub>>();
+            mockHubContext.SetupGet(hubContext => hubContext.Clients).Returns(mockClients.Object);
+
+
+            sut = new ProcessorService(mockHubContext.Object, mockMemoryService.Object);
+        }
+
+        [Fact]
+        //When....Should
+        public void When_Calling_ExecuteFile_Should_Result_All_Instruction_used()
+        {
+            //Arrange
+            //Prepare data to be testeds
+            //Se poate suprascrie logica din constructor specific pentru test
+
+            Memory.internalMemory[0] = "mov reg1,3";
+            Memory.instructionsNumber = 1;
+            mockMemoryService.Setup(x => x.LoadMemoryData()).Returns((ResultMemory)null);
+            //Act
+            //Call method needed
+            sut.ExecuteFile();
+
+
+            //Assert
+            //Very result
+            Assert.Equal(3, Processor.registerDictionary["reg1"]);
+        }
+    }
+}
