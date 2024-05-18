56cd6a3f476d734481b20116504e4c8d25a2d610
Adrian-Mihai Onofrei <59933980+WoozyDeveloper@users.noreply.github.com>
2024-05-18 17:54:41 +0300

    unit test jmp comp

---
+++ ProjectSQ.Tests/ProcessorServiceTests/ComparisonTests.cs
@@ -0,0 +1,98 @@
+﻿using Xunit;
+using FluentAssertions;
+using ProjectSQ.Services;
+using ProjectSQ.Interfaces.Memory;
+using Moq;
+using Microsoft.AspNetCore.SignalR;
+using ProjectSQ.Models;
+
+namespace ProjectSQ.Tests.ProcessorServiceTests
+{
+    public class ComparisonTests : IDisposable
+    {
+        private readonly ProcessorService _processorService;
+
+        public ComparisonTests()
+        {
+            Mock<IHubContext<RealTimeHub>> mockHubContext = new();
+            Mock<IMemoryService> mockMemoryService = new();
+            _processorService = new ProcessorService(mockHubContext.Object, mockMemoryService.Object);
+            _processorService.ResetData();
+        }
+
+        public void Dispose()
+        {
+            _processorService.ResetData();
+        }
+
+        [Fact]
+        public void Compare_SetsZeroFlag_WhenOperandsAreEqual()
+        {
+            // Arrange
+            const ushort value1 = 100;
+            const ushort value2 = 100;
+
+            // Act
+            _processorService.Compare(value1, value2);
+
+            // Assert
+            _processorService.ZeroFlag.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Compare_SetsCarryFlag_WhenFirstOperandIsGreater()
+        {
+            // Arrange
+            const ushort value1 = 200;
+            const ushort value2 = 100;
+
+            // Act
+            _processorService.Compare(value1, value2);
+
+            // Assert
+            _processorService.CarryFlag.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Compare_SetsCarryFlag_WhenFirstOperandIsLess()
+        {
+            // Arrange
+            const ushort value1 = 50;
+            const ushort value2 = 100;
+
+            // Act
+            _processorService.Compare(value1, value2);
+
+            // Assert
+            _processorService.CarryFlag.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Compare_SetsNegativeFlag_WhenFirstOperandIsLess()
+        {
+            // Arrange
+            const ushort value1 = 50;
+            const ushort value2 = 100;
+
+            // Act
+            _processorService.Compare(value1, value2);
+
+            // Assert
+            _processorService.NegativeFlag.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Compare_SetsNegativeFlag_WhenFirstOperandIsNotLess()
+        {
+            // Arrange
+            const ushort value1 = 150;
+            const ushort value2 = 100;
+
+            // Act
+            _processorService.Compare(value1, value2);
+
+            // Assert
+            _processorService.NegativeFlag.Should().BeFalse();
+        }
+    }
+}
---
+++ ProjectSQ.Tests/ProcessorServiceTests/JumpTests.cs
@@ -0,0 +1,102 @@
+﻿using Xunit;
+using FluentAssertions;
+using ProjectSQ.Services;
+using ProjectSQ.Interfaces.Memory;
+using Moq;
+using Microsoft.AspNetCore.SignalR;
+using ProjectSQ.Models;
+
+namespace ProjectSQ.Tests.ProcessorServiceTests
+{
+    public class JumpTests : IDisposable
+    {
+        private readonly ProcessorService _processorService;
+
+        public JumpTests()
+        {
+            Mock<IHubContext<RealTimeHub>> mockHubContext = new();
+            Mock<IMemoryService> mockMemoryService = new();
+            _processorService = new ProcessorService(mockHubContext.Object, mockMemoryService.Object);
+            _processorService.ResetData();
+        }
+
+        public void Dispose()
+        {
+            _processorService.ResetData();
+        }
+
+        [Fact]
+        public void UnconditionalJump_SetsProgramCounter()
+        {
+            // Arrange
+            const ushort targetAddress = 100;
+
+            // Act
+            var result = _processorService.UnconditionalJump(targetAddress);
+
+            // Assert
+            result.Should().BeTrue();
+            _processorService.ProgramCounter.Should().Be(targetAddress);
+        }
+
+        [Fact]
+        public void ConditionalJump_WhenZeroFlagSet_JumpsToTargetAddress()
+        {
+            // Arrange
+            const ushort targetAddress = 200;
+            _processorService.SetZeroFlag(true);
+
+            // Act
+            var result = _processorService.ConditionalJump(targetAddress, Condition.Zero);
+
+            // Assert
+            result.Should().BeTrue();
+            _processorService.ProgramCounter.Should().Be(targetAddress);
+        }
+
+        [Fact]
+        public void ConditionalJump_WhenZeroFlagNotSet_DoesNotJump()
+        {
+            // Arrange
+            const ushort targetAddress = 300;
+            _processorService.SetZeroFlag(false);
+
+            // Act
+            var result = _processorService.ConditionalJump(targetAddress, Condition.Zero);
+
+            // Assert
+            result.Should().BeFalse();
+            _processorService.ProgramCounter.Should().NotBe(targetAddress);
+        }
+
+        [Fact]
+        public void ConditionalJump_WhenCarryFlagSet_JumpsToTargetAddress()
+        {
+            // Arrange
+            const ushort targetAddress = 400;
+            _processorService.SetCarryFlag(true);
+
+            // Act
+            var result = _processorService.ConditionalJump(targetAddress, Condition.Carry);
+
+            // Assert
+            result.Should().BeTrue();
+            _processorService.ProgramCounter.Should().Be(targetAddress);
+        }
+
+        [Fact]
+        public void ConditionalJump_WhenCarryFlagNotSet_DoesNotJump()
+        {
+            // Arrange
+            const ushort targetAddress = 500;
+            _processorService.SetCarryFlag(false);
+
+            // Act
+            var result = _processorService.ConditionalJump(targetAddress, Condition.Carry);
+
+            // Assert
+            result.Should().BeFalse();
+            _processorService.ProgramCounter.Should().NotBe(targetAddress);
+        }
+    }
+}
